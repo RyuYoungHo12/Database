093dd02ba99a36945987de9452c33752bc55b303
dudghfb1 <dudghfb1@gmail.com>
2023-06-26 17:51:27 +0900

    ddddd

--- 연습문제4.sql
+++ 연습문제4.sql
@@ -38,6 +38,13 @@ INSERT INTO `Customer` VALUES (3, '장미란','대한민국 강원도','000-7000
 INSERT INTO `Customer` VALUES (4, '추신수','미국 클리블랜드','000-8000-0001');
 INSERT INTO `Customer` VALUES (5, '박세리','대한민국 대전', NULL);
 
+INSERT INTO `Publisher` VALUES ('굿스포츠', '051-153-1234');
+INSERT INTO `Publisher` VALUES ('나무수', '051-153-1235');
+INSERT INTO `Publisher` VALUES ('대한미디어', '051-153-1236');
+INSERT INTO `Publisher` VALUES ('이상미디어', '051-153-1237');
+INSERT INTO `Publisher` VALUES ('삼성당', '051-153-1238');
+INSERT INTO `Publisher` VALUES ('Pearson', '051-153-1239');
+
 INSERT INTO `Book` VALUES (1,'축구의 역사', '굿스포츠',7000);
 INSERT INTO `Book` VALUES (2,'축구아는 여자', '나무수',13000);
 INSERT INTO `Book` VALUES (3,'축구의 이해', '대한미디어',22000);
@@ -49,16 +56,6 @@ INSERT INTO `Book` VALUES (8,'야구를 부탁해', '이상미디어',13000);
 INSERT INTO `Book` VALUES (9,'올림픽 이야기', '삼성당',7500);
 INSERT INTO `Book` VALUES (10,'Olympic Champios', 'Pearson',13000);
 
-INSERT INTO `Order`	VALUES (1,1,1,6000,'2014-07-01');
-INSERT INTO `Order`	VALUES (2,1,3,21000,'2014-07-03');
-INSERT INTO `Order`	VALUES (3,2,5,8000,'2014-07-03');
-INSERT INTO `Order`	VALUES (4,3,6,6000,'2014-07-04');
-INSERT INTO `Order`	VALUES (5,4,7,20000,'2014-07-05');
-INSERT INTO `Order`	VALUES (6,1,2,12000,'2014-07-07');
-INSERT INTO `Order`	VALUES (7,4,8,13000,'2014-07-07');
-INSERT INTO `Order`	VALUES (8,3,10,12000,'2014-07-08');
-INSERT INTO `Order`	VALUES (9,2,10,7000,'2014-07-09');
-INSERT INTO `Order`	VALUES (10,3,8,13000,'2014-07-10');
 
 # 실습 4-4 [모든 회원의 회원번호, 이름, 주소를 조회하시오.]
 SELECT `custid`,`name`,`address` FROM `Customer`
@@ -227,4 +224,4 @@ INSERT INTO `Book` SET
 UPDATE `Customer` SET `address` = '대한민국 부산' WHERE `custId` =5;
 
 # 실습 4-46
-DELETE FROM `Customer` WHERE `custId` = 5;
+DELETE FROM `Customer` WHERE `custId` = 5;bookstore
